05b67c26249ab55447917183ebb288ce76f96032
Mehmet Durgun <m.durgun019@gmail.com>
2022-09-28 00:13:33 +0300

    GithubKisisel

---
+++ src/Github/KisiselKullanim.java
@@ -0,0 +1,15 @@
+package Github;
+
+public class KisiselKullanim {
+ /*
+       Kişisel kullanim icin aşama
+       1- git init /* bilgisayarı gitle ilişkilendirmek icin
+       2- git add . /* git klasorune tum dosyaları eklemek ıcın
+       3- git commit -m "VersiyonIsmi"  /* local repoda güncellemeleri aktifleştirmek icin (yeni versiyon)
+       ----Local islemler bitti-
+       4- git branch -m main /* branchimizi kisisel kullanimda main olarak editlemek icin
+       5- git remote add origin LİNKYAPIŞTIR /* uzaktaki bağlantıyı kurmak icin
+       6- git push -u
+        */
+
+}
